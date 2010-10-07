a1c5bc2382d223c6c471cb15e155ad0e19a0f853
Baldur Emilsson <baldur@moonbase.(none)>
2010-10-07 17:03:42 +0000

    something is working

--- src/is/nord/dutchess/CodenameDutchess.java
+++ src/is/nord/dutchess/CodenameDutchess.java
@@ -1,13 +1,263 @@
 package is.nord.dutchess;
 
-import android.app.Activity;
-import android.os.Bundle;
+import static org.anddev.andengine.extension.physics.box2d.util.constants.PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT;
 
-public class CodenameDutchess extends Activity {
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.primitive.Rectangle;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.Scene.IOnAreaTouchListener;
+import org.anddev.andengine.entity.scene.Scene.IOnSceneTouchListener;
+import org.anddev.andengine.entity.scene.Scene.ITouchArea;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.shape.Shape;
+import org.anddev.andengine.entity.sprite.AnimatedSprite;
+import org.anddev.andengine.entity.text.Text;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.extension.physics.box2d.PhysicsConnector;
+import org.anddev.andengine.extension.physics.box2d.PhysicsFactory;
+import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
+import org.anddev.andengine.input.touch.TouchEvent;
+import org.anddev.andengine.opengl.font.Font;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+import org.anddev.andengine.sensor.accelerometer.AccelerometerData;
+import org.anddev.andengine.sensor.accelerometer.IAccelerometerListener;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+import org.anddev.andengine.util.Debug;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+
+import android.app.Activity;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+public class CodenameDutchess extends BaseGameActivity implements IAccelerometerListener, IOnSceneTouchListener {
+	
+	
+	// Constants
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
+
+	// Fields
+	
+	private Texture mTexture;
+
+	private TiledTextureRegion mBoxFaceTextureRegion;
+	private TiledTextureRegion mCircleFaceTextureRegion;
+	private TiledTextureRegion mTriangleFaceTextureRegion;
+	private TiledTextureRegion mHexagonFaceTextureRegion;
+
+	private PhysicsWorld mPhysicsWorld;
+
+	private int mFaceCount = 0;
+
+	private Texture mFontTexture;
+	private Font mFont;
+	private Text mText;
+	
+	
     /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main);
+//    }
+
+	public void onLoadComplete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Engine onLoadEngine() {
+
+		Toast.makeText(this, "Scene has been loaded please wait", Toast.LENGTH_LONG).show();
+		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera));
+	}
+
+	public void onLoadResources() {
+		/* Textures. */
+		//Toast.makeText(this, "onLoadResource ok", Toast.LENGTH_LONG).show();
+		this.mTexture = new Texture(64, 128, TextureOptions.BILINEAR);
+		TextureRegionFactory.setAssetBasePath("gfx/");
+
+		 //TextureRegions.
+		
+		this.mBoxFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "face_box_tiled.png", 0, 0, 2, 1); // 64x32
+		this.mCircleFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "face_circle_tiled.png", 0, 32, 2, 1); // 64x32
+		this.mTriangleFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "face_triangle_tiled.png", 0, 64, 2, 1); // 64x32
+		this.mHexagonFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "face_hexagon_tiled.png", 0, 96, 2, 1); // 64x32
+		this.mEngine.getTextureManager().loadTexture(this.mTexture);
+		
+		//THIS B BUGGY
+		this.enableAccelerometerSensor(this);
+		//Toast.makeText(this, "onloadresources finished", Toast.LENGTH_LONG).show();
+		
+	}
+
+	public Scene onLoadScene() {
+		
+		Debug.d("testing");
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+
+		final Scene scene = new Scene(2);
+		scene.setBackground(new ColorBackground(1.0f, 0, 0));
+		
+		scene.setOnSceneTouchListener(this);
+		this.mPhysicsWorld = new PhysicsWorld(new Vector2(0, SensorManager.GRAVITY_EARTH), false);
+
+		final Shape ground = new Rectangle(0, CAMERA_HEIGHT - 2, CAMERA_WIDTH, 2);
+		final Shape roof = new Rectangle(0, 0, CAMERA_WIDTH, 2);
+		final Shape left = new Rectangle(0, 0, 2, CAMERA_HEIGHT);
+		final Shape right = new Rectangle(CAMERA_WIDTH - 2, 0, 2, CAMERA_HEIGHT);
+
+		final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 0.5f, 0.5f);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, ground, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, roof, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, left, BodyType.StaticBody, wallFixtureDef);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, right, BodyType.StaticBody, wallFixtureDef);
+
+		scene.getBottomLayer().addEntity(ground);
+		scene.getBottomLayer().addEntity(roof);
+		scene.getBottomLayer().addEntity(left);
+		scene.getBottomLayer().addEntity(right);
+
+		scene.registerUpdateHandler(this.mPhysicsWorld);
+
+		return scene;
+	}
+	
+	public void onAccelerometerChanged(AccelerometerData pAccelerometerData) {
+		this.mPhysicsWorld.setGravity(new Vector2(pAccelerometerData.getY(), pAccelerometerData.getX()));
+		
+	}
+	
+	private void addFace(final float pX, final float pY) {
+		final Scene scene = this.mEngine.getScene();
+
+		this.mFaceCount++;
+		Debug.d("Faces: " + this.mFaceCount);
+
+		final AnimatedSprite face;
+		final Body body;
+
+		final FixtureDef objectFixtureDef = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f);
+
+		if(this.mFaceCount % 4 == 0) {
+			face = new AnimatedSprite(pX, pY, this.mBoxFaceTextureRegion);
+			body = PhysicsFactory.createBoxBody(this.mPhysicsWorld, face, BodyType.DynamicBody, objectFixtureDef);
+		} else if (this.mFaceCount % 4 == 1) {
+			face = new AnimatedSprite(pX, pY, this.mCircleFaceTextureRegion);
+			body = PhysicsFactory.createCircleBody(this.mPhysicsWorld, face, BodyType.DynamicBody, objectFixtureDef);
+		} else if (this.mFaceCount % 4 == 2) {
+			face = new AnimatedSprite(pX, pY, this.mTriangleFaceTextureRegion);
+			body = CodenameDutchess.createTriangleBody(this.mPhysicsWorld, face, BodyType.DynamicBody, objectFixtureDef);
+		} else {
+			face = new AnimatedSprite(pX, pY, this.mHexagonFaceTextureRegion);
+			body = CodenameDutchess.createHexagonBody(this.mPhysicsWorld, face, BodyType.DynamicBody, objectFixtureDef);
+		}
+
+		face.animate(200);
+		face.setUpdatePhysics(false);
+
+		scene.getTopLayer().addEntity(face);
+		this.mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(face, body, true, true, false, false));
+	}
+	
+	/**
+	 * Creates a {@link Body} based on a {@link PolygonShape} in the form of a triangle:
+	 * <pre>
+	 *  /\
+	 * /__\
+	 * </pre>
+	 */
+	private static Body createTriangleBody(final PhysicsWorld pPhysicsWorld, final Shape pShape, final BodyType pBodyType, final FixtureDef pFixtureDef) {
+		/* Remember that the vertices are relative to the center-coordinates of the Shape. */
+		final float halfWidth = pShape.getWidthScaled() * 0.5f / PIXEL_TO_METER_RATIO_DEFAULT;
+		final float halfHeight = pShape.getHeightScaled() * 0.5f / PIXEL_TO_METER_RATIO_DEFAULT;
+
+		final float top = -halfHeight;
+		final float bottom = halfHeight;
+		final float left = -halfHeight;
+		final float centerX = 0;
+		final float right = halfWidth;
+
+		final Vector2[] vertices = {
+				new Vector2(centerX, top),
+				new Vector2(right, bottom),
+				new Vector2(left, bottom)
+		};
+
+		return PhysicsFactory.createPolygonBody(pPhysicsWorld, pShape, vertices, pBodyType, pFixtureDef);
+	}
+
+	/**
+	 * Creates a {@link Body} based on a {@link PolygonShape} in the form of a hexagon:
+	 * <pre>
+	 *  /\
+	 * /  \
+	 * |  |
+	 * |  |
+	 * \  /
+	 *  \/
+	 * </pre>
+	 */
+	private static Body createHexagonBody(final PhysicsWorld pPhysicsWorld, final Shape pShape, final BodyType pBodyType, final FixtureDef pFixtureDef) {
+		/* Remember that the vertices are relative to the center-coordinates of the Shape. */
+		final float halfWidth = pShape.getWidthScaled() * 0.5f / PIXEL_TO_METER_RATIO_DEFAULT;
+		final float halfHeight = pShape.getHeightScaled() * 0.5f / PIXEL_TO_METER_RATIO_DEFAULT;
+
+		/* The top and bottom vertex of the hexagon are on the bottom and top of hexagon-sprite. */
+		final float top = -halfHeight;
+		final float bottom = halfHeight;
+
+		final float centerX = 0;
+
+		/* The left and right vertices of the heaxgon are not on the edge of the hexagon-sprite, so we need to inset them a little. */
+		final float left = -halfWidth + 2.5f / PIXEL_TO_METER_RATIO_DEFAULT;
+		final float right = halfWidth - 2.5f / PIXEL_TO_METER_RATIO_DEFAULT;
+		final float higher = top + 8.25f / PIXEL_TO_METER_RATIO_DEFAULT;
+		final float lower = bottom - 8.25f / PIXEL_TO_METER_RATIO_DEFAULT;
+
+		final Vector2[] vertices = {
+				new Vector2(centerX, top),
+				new Vector2(right, higher),
+				new Vector2(right, lower),
+				new Vector2(centerX, bottom),
+				new Vector2(left, lower),
+				new Vector2(left, higher)
+		};
+
+		return PhysicsFactory.createPolygonBody(pPhysicsWorld, pShape, vertices, pBodyType, pFixtureDef);
+	}
+	
+
+	public boolean onSceneTouchEvent(final Scene pScene, final TouchEvent pSceneTouchEvent) {
+		if(this.mPhysicsWorld != null) {
+			if(pSceneTouchEvent.getAction() == MotionEvent.ACTION_DOWN) {
+				this.runOnUpdateThread(new Runnable() {
+					
+					public void run() {
+						CodenameDutchess.this.addFace(pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
+					}
+				});
+				return true;
+			}
+		}
+		return false;
+	}
+
+	
 }
