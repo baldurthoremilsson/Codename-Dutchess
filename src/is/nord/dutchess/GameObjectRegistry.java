076b5c4c6a3c330cb3cb174b4b76dfc4b1617a6e
gunnarrb <gunnarrb@gmail.com>
2010-11-09 00:43:27 +0000

    Abstracting, sprinting, not done yet

---
+++ src/is/nord/dutchess/GameObjectRegistry.java
@@ -0,0 +1,76 @@
+package is.nord.dutchess;
+
+import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+
+import com.badlogic.gdx.physics.box2d.Body;
+
+/**
+ * 
+ * @author gunnarr
+ *
+ *	This class holds information regarding each game object which is required for the object's construction. 
+ */
+public class GameObjectRegistry {
+	private PhysicsWorld phisycs;
+	
+	private TextureRegion agentTextureRegion;
+	private TextureRegion wallTextureRegion;
+	private TextureRegion coinTextureRegion;
+	
+	private Body agentBody;
+	
+	/*
+	 * Usage:	GameObjectRegistry gor = new GameObjectRegistry(physicsworld);
+	 * Pre:		physicsworld is of type PhysicsWorld and dictates the physics for the game objects
+	 * Post:	gor is a new GameObjectRegistry affiliated with physicsworld. 
+	 */
+	public GameObjectRegistry(PhysicsWorld physicsEngine)
+	{
+		this.phisycs = physicsEngine;
+	}
+	
+	/* Setters */
+	public void setAgentTextureRegion(TextureRegion agentTextRegion)
+	{
+		this.agentTextureRegion = agentTextRegion;
+	}
+	
+	public void setWallTextureRegion(TextureRegion wallTextRegion)
+	{
+		this.wallTextureRegion = wallTextRegion;
+	}
+	
+	public void setCoinTextureRegion(TextureRegion coinTextRegion)
+	{
+		this.coinTextureRegion = coinTextRegion;
+	}
+	
+	public void setAgentBody(Body agentBody)
+	{
+		this.agentBody = agentBody;
+	}
+	
+	/* Getters */
+	public TextureRegion getAgentTextureRegion()
+	{
+		return this.agentTextureRegion;
+	}
+	
+	public TextureRegion getWallTextureRegion()
+	{
+		return this.wallTextureRegion;
+	}
+	
+	public TextureRegion getCoinTextureRegion()
+	{
+		return this.coinTextureRegion;
+	}
+	
+	public Body getAgentBody()
+	{
+		return this.agentBody;
+		
+	}
+
+}
